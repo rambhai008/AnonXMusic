47e30c5d41c51a076c9aeee040878d70164b7a53
rambhai008 <156694001+rambhai008@users.noreply.github.com>
2024-01-31 11:16:23 +0530

    Update nwaifu.py
    Signed-off-by: rambhai008 <156694001+rambhai008@users.noreply.github.com>

--- AnonXMusic/plugins/rias/nwaifu.py
+++ AnonXMusic/plugins/rias/nwaifu.py
@@ -1,21 +1,31 @@
 from pyrogram import Client, filters
 import requests
 import random
+import asyncio
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup  
 from AnonXMusic import app
+
+async def delete_message_after_timeout(message, timeout):
+    await asyncio.sleep(timeout)
+    await message.delete()
 
 regex_photo = ["waifu"]
 pht = random.choice(regex_photo)
 url = f"https://api.waifu.pics/nsfw/{pht}"
 
 @app.on_message(filters.command("nwaifu"))
-def get_waifu(client, message):
+async def get_waifu(client, message):
     response = requests.get(url).json()
-    up = response['url']
+    up = response.get('url')
+
     if up:
-        button = [[InlineKeyboardButton("SUPPORT", url=f"https://t.me/LustxSupport")]]
+        button = [[InlineKeyboardButton("SUPPORT", url="https://t.me/LustxSupport")]]
         markup = InlineKeyboardMarkup(button)
-        message.reply_photo(up,caption="BY @riasxrobot",reply_markup=markup)
-    else:
-        message.reply("Request failed try /again")
         
+        sent_message = await message.reply_photo(up, caption="BY @riasxrobot ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ɪɴ 30 ꜱᴇᴄᴏɴᴅ", reply_markup=markup)
+        
+        # Delete the sent message after 1 minute
+        asyncio.create_task(delete_message_after_timeout(sent_message, 30))
+    else:
+        await message.reply("Request failed, try /again")
+
